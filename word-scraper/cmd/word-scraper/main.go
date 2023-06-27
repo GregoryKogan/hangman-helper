f9184ffb40fa96626f538d3d06a67777be3fd708
GregoryKogan <grisha.koganovskiy@gmail.com>
2023-06-28 00:28:54 +0300

    formatting

--- word-scraper/cmd/word-scraper/main.go
+++ word-scraper/cmd/word-scraper/main.go
@@ -46,7 +46,9 @@ func main() {
 		}
 	})
 
-	c.Visit("https://ru.wiktionary.org/w/index.php?title=%D0%9A%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D1%8F:%D0%A0%D1%83%D1%81%D1%81%D0%BA%D0%B8%D0%B5_%D1%81%D1%83%D1%89%D0%B5%D1%81%D1%82%D0%B2%D0%B8%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D1%8B%D0%B5")
+	c.Visit(
+		"https://ru.wiktionary.org/w/index.php?title=%D0%9A%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D1%8F:%D0%A0%D1%83%D1%81%D1%81%D0%BA%D0%B8%D0%B5_%D1%81%D1%83%D1%89%D0%B5%D1%81%D1%82%D0%B2%D0%B8%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D1%8B%D0%B5",
+	)
 
 	sort.Strings(words)
 
